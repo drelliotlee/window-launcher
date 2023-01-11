aac35dffd284df9b6d84f6c9310fcab5b02a4878
Elliot Lee <drelliotlee@gmail.com>
2023-01-11 15:24:24 -0500

    Working as of Jan 11

--- windowLauncher.py
+++ windowLauncher.py
@@ -9,84 +9,49 @@ import time
 import os
 import re
 
-class WindowMgr:
-    """Encapsulates some calls to the winapi for window management"""
-    def __init__(self):
-        self._handle = None
+def moveWindowByWildcard(hwnd, tup):
+	'''
+	tup[0] is wildcard
+	tup[0] is x
+	tup[0] is y
+	tup[0] is width
+	tup[0] is height
+	'''
+	if tup[0] in win32gui.GetWindowText(hwnd):
+		win32gui.MoveWindow(hwnd, tup[1], tup[2], tup[3], tup[4], True)
 
-    def _window_enum_callback(self, hwnd, wildcard):
-        '''Pass to win32gui.EnumWindows() to check all the opened windows'''
-        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) != None:
-            self._handle = hwnd
+subprocess.Popen(
+	[r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+	'-app=https://ticktick.com/webapp/#p/63b6330c8f08f73ded0afb78/tasks/63b63d8f56148929e0849496'],
+	)
+subprocess.Popen(
+	[r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+	f'--app=https://mail.google.com/mail/u/0/?shva=1#inbox'])
+subprocess.Popen(
+	[r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+	f'--app=https://chat.openai.com/chat'])
+subprocess.call(
+	[r"C:\Windows\explorer.exe",
+	r"C:\Users\Elliot\Desktop"],
+	shell=True)
+subprocess.call(
+	[r"C:\Users\Elliot\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+subprocess.call(
+	["start",
+	r"C:\Users\Elliot\AppData\Local\Obsidian\Obsidian.exe"],
+	shell=True)
+subprocess.call(
+	[r"C:\Program Files\Google\Chrome\Application\chrome.exe"])
 
-    def find_window_wildcard(self, wildcard):
-        self._handle = None
-        win32gui.EnumWindows(self._window_enum_callback, wildcard)
-
-    def move_window(self, top, left, width, height):
-        win32gui.MoveWindow(self._handle, top, left, width, height, True)
-
-def createAndMoveChrome(url:str, name:str, x:int, y:int, width:int, height:int):
-    subprocess.Popen([
-        r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-        f'--app={url}'
-    ])
-    time.sleep(2)
-    w = WindowMgr()
-    w.find_window_wildcard(name)
-    w.move_window(x, y, width, height)
-
-
-createAndMoveChrome(
-    "https://ticktick.com/webapp/#p/63b6330c8f08f73ded0afb78/tasks/63b63d8f56148929e0849496",
-    ".*TickTick.*",
-    4049, 0, 438, 1059
-)
-
-createAndMoveChrome(
-    "https://mail.google.com/mail/u/0/?shva=1#inbox",
-    ".*(Gmail|Inbox).*",
-    3214, 0, 849, 462
-)
-
-createAndMoveChrome(
-    "https://chat.openai.com/chat",
-    ".*(Just a moment|New chat).*",
-    2553, 0, 675, 1057
-)
-
-subprocess.call([r"C:\Windows\explorer.exe",r"C:\Users\Elliot\Desktop"], shell=True)
-time.sleep(2)
-w = WindowMgr()
-w.find_window_wildcard(".*Desktop.*")
-w.move_window(3214, 455, 849, 602)
-
-subprocess.call([r"C:\Users\Elliot\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
-time.sleep(2)
-w = WindowMgr()
-w.find_window_wildcard(".*Visual Studio Code.*")
-w.move_window(0,0,728,1050)
-
-subprocess.call(["start", r"C:\Users\Elliot\AppData\Local\Obsidian\Obsidian.exe"], shell=True)
-time.sleep(4)
-w=win32gui.FindWindow(None, 'python - Obsidian - Obsidian v1.1.9')
-win32gui.MoveWindow(w, 1797,0,763,1050, True)
-# w.find_window_wildcard(".*Python.*")
-# w.move_window(1797,0,763,1050)
-
-subprocess.call([r"C:\Program Files\Google\Chrome\Application\chrome.exe"])
-time.sleep(2)
-w = WindowMgr()
-w.find_window_wildcard("New Tab")
-w.move_window(0,0,728,1050)
-
-# CODE TO FIND NAMES OF ALL OPEN WINDOWS
-# import win32gui
-# def winEnumHandler( hwnd, ctx ):
-#     if win32gui.IsWindowVisible( hwnd ):
-#         print (hex(hwnd), win32gui.GetWindowText( hwnd ))
-
-# win32gui.EnumWindows( winEnumHandler, None )
+#EnumWindows is like a 'for loop' for the win32gui package.
+# it runs the function on every window, along with the provided 2nd argument
+win32gui.EnumWindows(moveWindowByWildcard, ('TickTick',4049, 0, 438, 1059))
+win32gui.EnumWindows(moveWindowByWildcard, ('Gmail',3214, 0, 849, 462))
+win32gui.EnumWindows(moveWindowByWildcard, ('New Chat',2553, 0, 675, 1057))
+win32gui.EnumWindows(moveWindowByWildcard, ('Desktop',3214, 455, 849, 602))
+win32gui.EnumWindows(moveWindowByWildcard, ('Visual Studio Code',0,0,728,1050))
+win32gui.EnumWindows(moveWindowByWildcard, ('Obsidian',1797,0,763,1050))
+win32gui.EnumWindows(moveWindowByWildcard, ('New Tab',721,0,1083,1057))
 
 # CODE TO FIND SIZE AND POS OF MONITORS
 # import win32gui
@@ -99,9 +64,7 @@ w.move_window(0,0,728,1050)
 #     name = win32gui.GetWindowText(hwnd)
 #     if w>10: # want to ignore ghost/invisible windows
 #         if len(name)>0: # want to ignore ghost/invisible windows
-#             print(win32gui.GetWindowText(hwnd))
+#             print("Window %s:" % win32gui.GetWindowText(hwnd))
 #             print("\tLocation: (%d, %d)" % (x, y))
 #             print("\t    Size: (%d, %d)" % (w, h))
 # win32gui.EnumWindows(callback, None)
-
-
